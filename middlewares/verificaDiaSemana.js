ae0f7625b7c91a5e6c1005282321de48e70bf430
kasaoli <kauansantos60@gmail.com>
2026-03-22 13:54:13 -0300

    corrigindo bugs

--- middlewares/verificaDiaSemana.js
+++ middlewares/verificaDiaSemana.js
@@ -2,6 +2,6 @@ module.exports = (req, res, next) => {
     const diaDaSemana = new Date().getDay(); // 0 = Domingo, 6 = Sábado
     /*if (diaDaSemana === 0 || diaDaSemana === 6) {
         return res.status(403).json({ erro: "A API só funciona de segunda a sexta-feira." });
-    }
-    next();*/
+    }*/
+    next();
 };
